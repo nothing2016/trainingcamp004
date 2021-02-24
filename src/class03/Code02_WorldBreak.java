9a7c97f03ba858bf0d96ad9be4ee86dcae99cc93
oudaming <805269294@qq.com>
2021-02-24 23:40:31 +0800

    多个单词变成一个大单词问题

--- src/class03/Code02_WorldBreak.java
+++ src/class03/Code02_WorldBreak.java
@@ -1,7 +1,12 @@
 package class03;
 
 import java.util.HashSet;
-
+/*
+ * 多个单词变成一个大单词问题
+ * 假设所有字符都是小写字母. 大字符串是str. arr是去重的单词表, 每个单词都不是空字符串且可以使用任意次.
+ * 使用arr中的单词有多少种拼接str的方式. 返回方法数.
+ *
+ */
 public class Code02_WorldBreak {
 	/*
 	 * 
